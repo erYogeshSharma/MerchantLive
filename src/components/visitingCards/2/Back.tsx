d476fa046007ded127a43dd3c7cd372adcf00b22
= <=>
2024-06-05 14:30:02 +0530

    refactor: Remove unnecessary React import statements in visiting card components

--- src/components/visitingCards/2/Back.tsx
+++ src/components/visitingCards/2/Back.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CardTwoBack = () => {
   return <div>back</div>;
 };
--- src/components/visitingCards/2/Front.tsx
+++ src/components/visitingCards/2/Front.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CardTwoFront = () => {
   return <div>front</div>;
 };
